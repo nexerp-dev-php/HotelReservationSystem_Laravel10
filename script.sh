50e0088897e913ed1752543a742942350e22ec10
nexerp-dev-php <nexerp.dev.php@gmail.com>
2025-10-07 00:00:16 +0800

    Customized registration page

---
+++ script.sh
@@ -0,0 +1,11 @@
+docker exec -it laravel10-app php artisan key:generate
+docker exec -it laravel10-app php artisan migrate
+
+docker exec -it laravel10-app php artisan config:cache
+docker exec -it laravel10-app php artisan route:cache
+
+docker exec -it laravel10-app chmod -R 775 storage bootstrap/cache
+docker exec -it laravel10-app chown -R www-data:www-data storage bootstrap/cache
+
+docker stop laravel10-app
+docker start laravel10-app
