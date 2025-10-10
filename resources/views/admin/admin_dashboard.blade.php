df1433663cfe908fe1d790692ad920d7ba9b4a44
nexerp-dev-php <nexerp.dev.php@gmail.com>
2025-10-11 01:33:54 +0800

    Added form validation onto Team module

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -78,6 +78,7 @@
 	<!--Sweet alert-->
 	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 	<script src="{{ asset('backend/assets/js/code.js') }}"></script>
+	<script src="{{ asset('backend/assets/js/validate.min.js') }}"></script>
 	
 	<!--Toaster-->
 	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
